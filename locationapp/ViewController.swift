3e9682addf487de4890b2fd8c5abc365ab751c62
COE-009 <coe-009@coe-009s-imac.local>
2019-09-18 15:17:37 +0530

    very happy to do this task!

--- locationapp/ViewController.swift
+++ locationapp/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+    var myDataArray=["India","Australia","America","Canada","Russia","Pakistan","England","New_zeland","SouthAfrika"]
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return myDataArray.count
+    }
+    
 
+    @IBOutlet weak var MyToolbar: UIToolbar!
+    
+    @IBOutlet weak var MyPicker: UIPickerView!
+    
+    @IBOutlet weak var Selectedstate: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.MyPicker.delegate=self
+        self.MyPicker.isHidden=true
+        self.MyToolbar.isHidden=true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
+    
 
-
+    @IBAction func BtnLocation(_ sender: Any) {
+        self.MyToolbar.isHidden=false
+        self.MyPicker.isHidden=false
+    }
+    
+    @IBAction func Done(_ sender: Any) {
+        self.Selectedstate.text=self.myDataArray[self.MyPicker.selectedRow(inComponent: 0)]
+        self.MyPicker.isHidden=true
+        self.MyToolbar.isHidden=true
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.myDataArray[row]
+    }
+   
 }
 
